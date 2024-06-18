0bf56b40584a012861534a158298aa18d183509c
Aferpal <fernandezpalomoabel@gmail.com>
2024-06-18 20:47:46 +0200

    Adding new default models for tests

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main()
     Game g;
     g.readShapeFromFilePath("./data/cube.txt");
     g.readShapeFromFilePath("./data/triangle.txt");
+    g.readShapeFromFilePath("./data/cube2.txt");
+    g.readShapeFromFilePath("./data/triangle2.txt");
     Interface i = {g, 1000, 1000};
     return 0;
 }
